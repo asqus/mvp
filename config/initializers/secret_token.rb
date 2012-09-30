1e6b2731ec7ea537b24b8994a1b1aff5d90d21e0
Adam Williams <kainolophobia@gmail.com>
2012-09-30 17:29:34 -0400

    initial rails stuff 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvp::Application.config.secret_token = '826f1d36035fccee81460d5cb4c1748262da629e02033b7f76da472e48c366281b6a107840134cd022509e2e3cbde5104f1a8f39fb1e2081c755ec3e5637b1f6'
