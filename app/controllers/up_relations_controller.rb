9af52167a8143c0f0fa1e943011e017e426e2ac6
Andrew Kuntz <kuntzand@gmail.com>
2012-11-05 00:00:54 -0500

    started rating system for polls, polls index view is broken

---
+++ app/controllers/up_relations_controller.rb
@@ -0,0 +1,34 @@
+class UpRelationsController < ApplicationController
+	def create
+		@poll = Poll.find(params[:up_relation][:poll_id])
+		@updown = params[:up_relation][:updown]
+		current_user.vote!(@poll, @updown)
+
+		respond_to do |format|
+			format.html { redirect_to polls_path }
+			format.js { render "render"}
+		end
+	end
+
+	def update
+		@up = UpRelation.find(params[:id])
+		@poll = Poll.find(@up.poll_id)
+		@up.update_attributes(params[:up_relation])
+		
+		respond_to do |format|
+			format.html { redirect_to polls_path }
+			format.js { render "render"}
+		end
+	end
+
+	def destroy
+		@up = UpRelation.find(params[:id])
+		@poll = Poll.find(@up.poll_id)
+		@up.destroy
+
+		respond_to do |format|
+			format.html { redirect_to polls_path }
+			format.js { render "render"}
+		end
+	end
+end
---
+++ app/models/up_relation.rb
@@ -0,0 +1,9 @@
+class UpRelation < ActiveRecord::Base
+  attr_accessible :poll_id, :user_id, :updown
+
+  belongs_to :poll, class_name: "Poll"
+  belongs_to :user, class_name: "User"
+
+  validates :poll_id, presence: true
+  validates :user_id, presence: true
+end
