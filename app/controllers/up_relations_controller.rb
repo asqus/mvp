741a52b50c529c27f277f651e59cf556ff049f71
kschow <kschow48@gmail.com>
2012-11-24 18:19:02 -0500

    organized some files and added jqplot to project

--- app/controllers/up_relations_controller.rb
+++ app/controllers/up_relations_controller.rb
@@ -1,44 +1,45 @@
 class UpRelationsController < ApplicationController
-	def create
-		@poll = Poll.find(params[:up_relation][:poll_id])
-		@updown = params[:up_relation][:updown]
-		current_user.votepoll!(@poll, @updown)
+  def create
+    @poll = Poll.find(params[:up_relation][:poll_id])
+    @updown = params[:up_relation][:updown]
+    current_user.votepoll!(@poll, @updown)
 
-		respond_to do |format|
-			format.html { redirect_to polls_path }
-			format.js { render "render"}
-		end
-	end
+    respond_to do |format|
+      format.html { redirect_to polls_path }
+      format.js { render "render"}
+    end
+  end
 
   def answer
     @uprel = UpRelation.find_or_create_by_user_id_and_poll_id(current_user.id, params[:poll_id])
     @uprel.update_attributes(:answerValue => params[:answer])
-    poll = Poll.find(params[:poll_id])
-		respond_to do |format|
-			format.html { redirect_to poll }
-			format.js 
-		end
+    @poll = Poll.find(params[:poll_id])
+
+    respond_to do |format|
+      format.html { redirect_to @poll }
+      format.js {render "renderPollAnswers"}
+    end
   end
 
-	def update
-		@up = UpRelation.find(params[:id])
-		@poll = Poll.find(@up.poll_id)
-		@up.update_attributes(params[:up_relation])
-		
-		respond_to do |format|
-			format.html { redirect_to polls_path }
-			format.js { render "render"}
-		end
-	end
+  def update
+    @up = UpRelation.find(params[:id])
+    @poll = Poll.find(@up.poll_id)
+    @up.update_attributes(params[:up_relation])
+    
+    respond_to do |format|
+      format.html { redirect_to polls_path }
+      format.js { render "render"}
+    end
+  end
 
-	def destroy
-		@up = UpRelation.find(params[:id])
-		@poll = Poll.find(@up.poll_id)
-		@up.destroy
+  def destroy
+    @up = UpRelation.find(params[:id])
+    @poll = Poll.find(@up.poll_id)
+    @up.destroy
 
-		respond_to do |format|
-			format.html { redirect_to polls_path }
-			format.js { render "render"}
-		end
-	end
+    respond_to do |format|
+      format.html { redirect_to polls_path }
+      format.js { render "render"}
+    end
+  end
 end
